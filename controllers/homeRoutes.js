f0a5664f4d47aa5b01a5c69960cd289e40b56e5d
denise-alvarez <denisealvarez3@gmail.com>
2022-08-29 17:28:23 -0700

    update views and some routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,3 +22,61 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      attributes: {
+        exclude: ["user_id", "updatedAt"],
+      },
+
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ["password", "createdAt"],
+          },
+        },
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: { exclude: ["password"] },
+          },
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+      console.log("status", req.session.logged_in);
+      res.render("single-post", {
+        payload: { posts: [post], logged_in: req.session.logged_in },
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+module.exports = router;
