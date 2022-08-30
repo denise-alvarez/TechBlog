8450ddfff106d718fe811b4b3b582a7b49132a36
denise-alvarez <denisealvarez3@gmail.com>
2022-08-29 19:29:00 -0700

    update js

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async function (event) {
+  event.preventDefault();
+
+  const postId = document.querySelector("#post-id").dataset.postId;
+  const content = document.querySelector('textarea[name="comment-body"]').value;
+
+  console.log("post id: ", postId);
+  console.log("content: ", content);
+
+  if (content) {
+    await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id: postId,
+        content,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    document.location.reload();
+  }
+};
