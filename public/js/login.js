a78496d3d99aa43d6b108258e89eafa59d89f689
denise-alvarez <denisealvarez3@gmail.com>
2022-08-31 16:23:09 -0700

    update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -4,26 +4,27 @@ const loginFormHandler = async function(event) {
     const usernameEl = document.querySelector('#username-field');
     const passwordEl = document.querySelector('#password-field');
   
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: usernameEl.value,
-        password: passwordEl.value,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
-    response.json().then(data => console.log(data));
+    if (username && password) {
+   
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
   
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to login');
+      if (response.ok) {
+       
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     }
-  
-    
   };
   
   document
-    .querySelector('#login-btn')
-    .addEventListener('click', loginFormHandler);
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
